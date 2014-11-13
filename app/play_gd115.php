6ba5bd04e8e43f6acf41d0d4e2c1ffac72812311
Feely <feelysong@gmail.com>
2014-11-13 17:03:56 +0800

    V2.0

--- app/play_gd115.php
+++ app/play_gd115.php
@@ -649,7 +649,7 @@ if($nums==0){
                                     <a class="gct_menu_yl" href='history_code.php?id=8' target="_blank"></a>                                </div>
                             </div>
                             <div class="gct_r">
-                                <h3>广东十一选五  第 <b><span class=nn id="lt_gethistorycode"><?=substr($rowc['issue'],-8)?></span> </b> 期 
+                                <h3>广东十一选五  第 <b><span class=nn id="lt_gethistorycode"><?=substr($rowc['issue'],-9)?></span> </b> 期 
 								<span id="lt_opentimebox" style="display:none;">&nbsp;&nbsp;<span id="waitopendesc">等待开奖</span>&nbsp;<span style="color:#F9CE46;" id="lt_opentimeleft" ></span></span><span id="lt_opentimebox2" style="display:none; color:#F9CE46;"><strong>&nbsp;&nbsp;正在开奖</strong></span></h3>
 								
 								<div style="display:none;" class="tad" id="showadvbox"><a href="promotion_center.php"><img src='images/v1/ad.jpg' border="0" /></a></div>
