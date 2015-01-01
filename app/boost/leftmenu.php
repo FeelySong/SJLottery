fff75a4512576bfebcb47c30ced8590118f8ac44
Feely <feelysong@gmail.com>
2015-01-01 16:53:57 +0800

    V2.0

--- app/boost/leftmenu.php
+++ app/boost/leftmenu.php
@@ -23,7 +23,7 @@ if($flag=="getmoney"){
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head><title>左侧菜单</title>
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta charset="UTF-8">
         <title>AdminLTE | Dashboard</title>
