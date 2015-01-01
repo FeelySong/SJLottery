7964a44d692264584779ce0053b18a90469d39ce
Feely <feelysong@gmail.com>
2015-01-01 18:11:38 +0800

    V2.0

--- app/boost/leftmenu.php
+++ app/boost/leftmenu.php
@@ -88,7 +88,7 @@ if($flag=="getmoney"){
 			$rsnewslist = mysql_query($sql);
 			while ($row = mysql_fetch_array($rsnewslist)){
 			?>
-                        <li class="fa fa-dashboard"><i class='<?=$row['name']?> (<?=$row['cname']?>)' ALT='<?=$row['name']?> (<?=$row['cname']?>)'><a target="rightside" href="../"."<?=$row['urls']?>"><?=$row['name']?></a></i>></li>
+                        <li class="fa fa-dashboard"><i class='<?=$row['name']?> (<?=$row['cname']?>)' ALT='<?=$row['name']?> (<?=$row['cname']?>)'></i><a target="rightside" href="../"."<?=$row['urls']?>"><?=$row['name']?></a></li>
                         <?php }?>
                             
                         <li class="active">
