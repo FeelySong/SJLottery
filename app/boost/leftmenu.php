7a9dbd9b26817dbcf3e308ab97361a39acd7df53
Feely <feelysong@gmail.com>
2015-01-02 21:37:51 +0800

    V2.0

--- app/boost/leftmenu.php
+++ app/boost/leftmenu.php
@@ -83,15 +83,15 @@ if($flag=="getmoney"){
                     <!-- /.search form -->
                     <!-- sidebar menu: : style can be found in sidebar.less -->
                     <ul class="sidebar-menu">
-                        <span>
+                        
                         <?php 
 			$sql="select * from ssc_set where zt=1 order by lid asc";
 			$rsnewslist = mysql_query($sql);
 			while ($row = mysql_fetch_array($rsnewslist)){
 			?>
-                            <li class="fa fa-dashboard"><i class='<?=$row['name']?> (<?=$row['cname']?>)' ALT='<?=$row['name']?> (<?=$row['cname']?>)'></i><a target="rightside" href="../<?=$row['urls']?>"><?=$row['name']?></a></li><li></li>
+                            <i class="fa fa-dashboard"><i class='<?=$row['name']?> (<?=$row['cname']?>)' ALT='<?=$row['name']?> (<?=$row['cname']?>)'></i><a target="right-side" href="../<?=$row['urls']?>"><?=$row['name']?></a></i><li></li>
                         <?php }?>
-                        </span>
+                        
                             
                         <li class="active">
                             <a href="index.html">
